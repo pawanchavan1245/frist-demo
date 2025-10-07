253448dbdc33a761260fdecd82c6105b6384fe2c
pawanchavan1245 <pawanchavan1245@gmail.com>
2025-10-07 22:14:08 +0530

    displaying the output of the datatypes

---
+++ Simple_datatypes.java
@@ -0,0 +1,14 @@
+public class Simple_datatypes{
+    public static void main(String []args){
+    
+int a=10;
+boolean val=true;
+char charactor='a';
+float length=12.3f;
+System.out.print(a);
+System.out.print(val);
+System.out.print(charactor);
+
+
+    }
+}
